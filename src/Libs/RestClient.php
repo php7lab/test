7dd22a710bf26b2376ee96fd67657dd75e202a4d
Vitaliy <theyamshikov@yandex.ru>
2020-02-16 23:28:16 +0600

    refacto

--- src/Libs/RestClient.php
+++ src/Libs/RestClient.php
@@ -9,37 +9,47 @@ use PhpLab\Core\Domain\Entities\DataProviderEntity;
 use PhpLab\Core\Enums\Http\HttpHeaderEnum;
 use PhpLab\Core\Enums\Http\HttpMethodEnum;
 use PhpLab\Core\Legacy\Yii\Helpers\ArrayHelper;
+use PhpLab\Core\Legacy\Yii\Helpers\FileHelper;
 use PhpLab\Test\Helpers\RestHelper;
 use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\CacheItem;
 
 class RestClient
 {
 
     private $guzzleClient;
     private $accept = 'application/json';
-    private $authToken;
     private $authUri = 'auth';
+    private $authCache;
+    private $currentAuth = [];
 
     public function __construct(Client $guzzleClient)
     {
         $this->guzzleClient = $guzzleClient;
+        $cacheDirectory = FileHelper::path($_ENV['CACHE_DIRECTORY']);
+        $this->authCache = new FilesystemAdapter('test', 0, $cacheDirectory);
     }
 
     public function authByLogin(string $login, string $password = 'Wwwqqq111') {
-        $response = $this->sendPost($this->authUri, [
+        $this->currentAuth = [
             'login' => $login,
             'password' => $password,
-        ]);
-        $this->authToken = RestHelper::getBodyAttribute($response, 'token');
+        ];
         return $this;
     }
 
-    public function setAuthToken(string $authToken) {
-        $this->authToken = $authToken;
+    public function logout() {
+        $this->currentAuth = [];
     }
 
-    public function getAuthToken() {
-        return $this->authToken;
+    public function setAuthToken(string $authToken) {
+
+        /** @var CacheItem $cacheItem */
+        $cacheItem = $this->authCache->getItem('token_by_login_' . $this->currentAuth['login']);
+        $cacheItem->set($authToken);
+        $this->authCache->save($cacheItem);
+        return $this;
     }
 
     public function sendOptions(string $uri, array $headers = []): ResponseInterface
@@ -85,18 +95,54 @@ class RestClient
         return $this->sendRequest(HttpMethodEnum::GET, $uri, $options);
     }
 
-    public function sendRequest(string $method, string $uri = '', array $options = []): ResponseInterface
+    public function sendRequest(string $method, string $uri = '', array $options = [], bool $refreshAuthToken = true): ResponseInterface
     {
         $options[RequestOptions::HEADERS]['Accept'] = $this->accept;
-        if($this->authToken) {
-            $options[RequestOptions::HEADERS][HttpHeaderEnum::AUTHORIZATION] = $this->authToken;
+        $authToken = $this->getAuthToken();
+        if($authToken) {
+            $options[RequestOptions::HEADERS][HttpHeaderEnum::AUTHORIZATION] = $authToken;
+        } else {
+            $refreshAuthToken = false;
         }
         try {
             $response = $this->guzzleClient->request($method, $uri, $options);
         } catch (RequestException $e) {
             $response = $e->getResponse();
+            if($response->getStatusCode() == 401 && $refreshAuthToken) {
+                //dd(401);
+                $this->authorization();
+                return $this->sendRequest($method, $uri, $options, false);
+            }
         }
         return $response;
+    }
+
+    private function getAuthToken(): ?string {
+
+        if(empty($this->currentAuth['login'])) {
+            return null;
+        }
+        /** @var CacheItem $cacheItem */
+        $cacheItem = $this->authCache->getItem('token_by_login_' . $this->currentAuth['login']);
+        $authToken = $cacheItem->get();
+
+        if($authToken) {
+            return $authToken;
+        } else {
+            return $this->authorization();
+        }
+    }
+
+    private function authorization() {
+        $clone = clone $this;
+        $clone->logout();
+        $response = $clone->sendPost($this->authUri, [
+            'login' => $this->currentAuth['login'],
+            'password' => $this->currentAuth['password'],
+        ]);
+        $authToken = RestHelper::getBodyAttribute($response, 'token');
+        $this->setAuthToken($authToken);
+        return $authToken;
     }
 
 }
