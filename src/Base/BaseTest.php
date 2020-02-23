9c61f89fd1ec8b824a5a1c92fd0e15a7c3c49474
Vitaliy <theyamshikov@yandex.ru>
2020-02-23 18:18:58 +0600

    clean

--- src/Base/BaseTest.php
+++ src/Base/BaseTest.php
@@ -2,24 +2,10 @@
 
 namespace PhpLab\Test\Base;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\RequestOptions;
 use PhpLab\Core\Domain\Exceptions\UnprocessibleEntityException;
 use PhpLab\Core\Domain\Helpers\EntityHelper;
-use PhpLab\Core\Helpers\InstanceHelper;
-use PhpLab\Core\Helpers\StringHelper;
-use PhpLab\Core\Legacy\Yii\Helpers\ArrayHelper;
-use PhpLab\Core\Enums\Http\HttpHeaderEnum;
-use PhpLab\Core\Enums\Http\HttpMethodEnum;
-use PhpLab\Core\Enums\Http\HttpStatusCodeEnum;
-use PhpLab\Test\Helpers\RestHelper;
 use PhpLab\Test\Libs\FixtureLoader;
-use PhpLab\Test\Libs\RestAssert;
-use PhpLab\Test\Libs\RestClient;
 use PHPUnit\Framework\TestCase;
-use Psr\Http\Message\ResponseInterface;
-use yii\test\Fixture;
 
 abstract class BaseTest extends TestCase
 {
@@ -33,7 +19,7 @@ abstract class BaseTest extends TestCase
     {
         parent::setUp();
         $fixtures = $this->fixtures();
-        if($fixtures) {
+        if ($fixtures) {
             $fixtureLoader = new FixtureLoader;
             $fixtureLoader->load($fixtures);
         }
