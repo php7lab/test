f7d46c3ab489e85a7f04139185a71332bcc75e40
Vitaliy <theyamshikov@yandex.ru>
2020-03-09 17:35:22 +0600

    refacto

--- src/Base/BaseRestWebTest.php
+++ src/Base/BaseRestWebTest.php
@@ -8,9 +8,26 @@ use Psr\Http\Message\ResponseInterface;
 abstract class BaseRestWebTest extends BaseRestTest
 {
 
+    protected function printHtmlContent(ResponseInterface $response = null) {
+        $this->printContent($response, 'strip_tags');
+    }
+
+    protected function printContent(ResponseInterface $response = null, string $filter = null) {
+        $content = $response->getBody()->getContents();
+        if($filter) {
+            $content = $filter($content);
+        }
+        dd($content);
+    }
+
     protected function getRestAssert(ResponseInterface $response = null): RestWebAssert
     {
         return new RestWebAssert($response);
     }
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->setBaseUrl($_ENV['WEB_URL']);
+    }
 }
