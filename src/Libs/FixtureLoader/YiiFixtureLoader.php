dea2b6e9d1f8f5e544e0803a6d46a495dea2320d
Vitaliy <theyamshikov@yandexru>
2020-06-02 15:46:09 +0600

    fix: recursive load depends of fixture

--- src/Libs/FixtureLoader/YiiFixtureLoader.php
+++ src/Libs/FixtureLoader/YiiFixtureLoader.php
@@ -20,24 +20,24 @@ class YiiFixtureLoader implements FixtureLoaderInterface
         if (empty($fixtures)) {
             return;
         }
-        foreach ($fixtures as $fixture) {
-            $this->loadFixture($fixture);
+        foreach ($fixtures as $fixtureClassName) {
+            $this->loadFixture($fixtureClassName);
         }
     }
 
-    private function loadFixture(string $fixture)
+    private function loadFixture(string $fixtureClassName)
     {
-        if (isset($this->loadedFixtures[$fixture])) {
+        if (isset($this->loadedFixtures[$fixtureClassName])) {
             return;
         }
+        $this->loadedFixtures[$fixtureClassName] = true;
         /** @var Fixture $fixtureInstance */
-        $fixtureInstance = InstanceHelper::ensure($fixture);
+        $fixtureInstance = InstanceHelper::ensure($fixtureClassName);
         if ($fixtureInstance->depends) {
             $this->loadFixtures($fixtureInstance->depends);
         }
         $fixtureInstance->unload();
         $fixtureInstance->load();
-        $this->loadedFixtures[$fixture] = true;
     }
 
 }
